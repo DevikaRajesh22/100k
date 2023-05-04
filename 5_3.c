0481a89a426b6a6dcca8101e68a92ea62be610db
DevikaRajesh22 <devikaraj699@gmail.com>
2023-05-04 13:44:12 +0530

    Day 5 completed

--- 5_3.c
+++ 5_3.c
@@ -1,6 +1,32 @@
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
+    int arr[100],i,j,temp,limit;
+    printf("Enter the size of an array: \n");
+    scanf("%d",&limit);
+    printf("Enter the values of the array: \n");
+    for(i=0;i<limit;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<limit-1;i++)
+    {
+        for(j=i+1;j<limit;j++)
+        {
+            if(arr[i]<arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    printf("Sorted array: \n");
+    for(i=0;i<limit;i++)
+    {
+        printf("%d",arr[i]);
+        printf(" ");
+    }
     return 0;
 }
