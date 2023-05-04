a14db0e6f673d842ea347b206ff8036816898c54
DevikaRajesh22 <devikaraj699@gmail.com>
2023-05-04 13:39:48 +0530

    Task 2 of day 5 completed

--- 5_2.c
+++ 5_2.c
@@ -2,5 +2,21 @@
 #include<stdio.h>
 int main()
 {
+    int arr[100],i,limit,count=0;
+    printf("Enter the size of an array: \n");
+    scanf("%d",&limit);
+    printf("Enter the values of array:\n");
+    for(i=0;i<limit;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<limit;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            count++;
+        }
+    }
+    printf("Number of even numbers in the given array is: %d",count);
     return 0;
 }
